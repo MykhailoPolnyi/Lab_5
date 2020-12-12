f5ae18a237389ec3e930e9220fdba501b3d1fb69
MykhailoPolnyi <mykhailo.polnyi.knm.2020@plnu.ua>
2020-12-12 23:33:14 +0200

    file Points.h contains declaration of class Points, Here is  fiields and methods of class with their descriprion:

--- Points.h
+++ Points.h
@@ -5,68 +5,23 @@
 
 using namespace std;
 
-
-
 class Point
 {
-	double x, y;
+	double x_coordinate, y_coordinate;
+
 public:
-	Point()
-	{
-		x = 0;
-		y = 0;
-	}
-	Point(double OX, double  OY)
-	{
-		x = OX;
-		this->y = OY;
-	}
+	
+	Point() : x_coordinate(0), y_coordinate(0) {};
+	
+	Point(double, double);
+	
 	virtual ~Point() {}
 
 	double GetX();
-	double GetY();
-	void GetCoord();
-	friend double Dist(Point*, Point*);
-};
-
-enum class Color { Black, Red, Green, Blue, White, Violet, Orange, Yellow};
-
-class Polynom
-{
 	
-	vector<Point> figure;
-	double per, l_d;
-	Color col;
-public:
-	Polynom()
-	{
-		per = 0;
-		l_d = 0;
-		col = Color(0);
-	}
-	Polynom(int point_num)
-	{
-		for (int i = 0; i < point_num; i++)
-		{
-			figure.push_back(Point((3 * sin(i)), (-4 * i)));
-		}
-		per = 0;
-		l_d = 0;
-		col = Color(0);
-	}
-	~Polynom() {};
-	//
-	void SetColor(int i);
-	double GetMax_Diag();
-	double GetPer();
-	void GetPolynom();
-
-	//
-	friend double Dist(Polynom* P, int a, int b);
-	friend void Sort_Pol_X(Polynom*);
-	friend void Sort_Pol_Y(Polynom*);
-	//
-	void Max_Diag();
-	void Perym();
+	double GetY();
+	
+	void GetCoordinates();
+	
+	friend double Distance(Point*, Point*);
 };
-
