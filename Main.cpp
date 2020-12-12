e27b949898a8952dd709dd58ba38637117c388b8
MykhailoPolnyi <mykhailo.polnyi.knm.2020@plnu.ua>
2020-12-12 23:08:49 +0200

    File Main.cpp contains some simple examples of using class Polynom

---
+++ Main.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include "Points.h"
+using namespace std;
+
+int main()
+{	
+	Polynom* figure_1 = new Polynom(5);
+	figure_1->SetColor(Color::Orange);
+	figure_1->GetPolynom();
+	figure_1->Perym();
+	cout << "Perimeter: " << figure_1->GetPer() << endl;
+
+	figure_1->Max_Diag();
+	cout << "Maximal diagonal: " << figure_1->GetMax_Diag() << endl;
+	Sort_Pol_X(figure_1);
+	figure_1->GetPolynom();
+	figure_1->Max_Diag();
+	cout << "Maximal diagonal: " << figure_1->GetMax_Diag() << endl;
+	Sort_Pol_Y(figure_1);
+	figure_1->GetPolynom();
+	figure_1->Max_Diag();
+	cout << "Maximal diagonal: " << figure_1->GetMax_Diag() << endl;
+	delete figure_1;
+}
